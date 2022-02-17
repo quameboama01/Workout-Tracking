00e922ea2f5f4aa4d0d32c3016a7a6a4d9914a8c
Andrews Boama-Secu <quameboama01@gmail.com>
2022-02-17 18:21:03 +0000

    final days commit

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 import requests
 
 APP_ID = "e9b1b420"
-API_KEY = "15eaf1aaa9c1ba07dae66ffecf3cef27"
+API_KEY = "f1deb0e16fe58a767548f4442e3a080e"
+
 GENDER = "male"
-WEIGHT_KG = 92.50
-HEIGHT_CM = 6.25
-AGE = 38
+WEIGHT_KG = "92.50"
+HEIGHT_CM = "6.25"
+AGE = "38"
 
 headers = {
-    "x-api-key": API_KEY,
     "x-app-id": APP_ID,
-    #"x-remote-user-id": "0"
+    "x-api-key": API_KEY,
+    "Content-Type": "application/json",
+    "x-remote-user-id": "0"
 }
 
 exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
 exercise_text =  input("Tell me what exercise you did today? ")
+
 exercise_config = {
-    "query":exercise_text,
+    "query": f"{exercise_text}",
     "gender": GENDER,
     "weight_kg": WEIGHT_KG,
     "height_cm": HEIGHT_CM,
@@ -24,8 +27,8 @@ exercise_config = {
 }
 
 response = requests.post(url=exercise_endpoint, json=exercise_config, headers=headers)
-#response.raise_for_status
+response.raise_for_status
 results = response.json()
 print(results)
 
-sheet_endpoint = "https://api.sheety.co/512493e17f966118373cd869f61856dd/workoutTracking/workouts"
+sheety_endpoint = "https://api.sheety.co/512493e17f966118373cd869f61856dd/workoutTracking/workouts"
