3045dee3d403c007d7fd3e6fcf302b8a2598cefd
Andrews Boama-Secu <quameboama01@gmail.com>
2022-02-23 16:26:41 +0000

    version 1.0 commit

--- main.py
+++ main.py
@@ -1,34 +1,53 @@
 import requests
+import datetime as dt
+from requests.auth import HTTPBasicAuth
 
-APP_ID = "e9b1b420"
-API_KEY = "f1deb0e16fe58a767548f4442e3a080e"
+APP_ID = 'e9b1b420'
+API_KEY = 'f1deb0e16fe58a767548f4442e3a080e'
 
-GENDER = "male"
-WEIGHT_KG = "92.50"
-HEIGHT_CM = "6.25"
-AGE = "38"
+GENDER = 'male'
+WEIGHT_KG = 92.5
+HEIGHT_CM = 168
+AGE = 38
 
 headers = {
     "x-app-id": APP_ID,
-    "x-api-key": API_KEY,
-    "Content-Type": "application/json",
-    "x-remote-user-id": "0"
+    "x-app-key": API_KEY,
 }
 
 exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
-exercise_text =  input("Tell me what exercise you did today? ")
+exercise_text = input("Tell me what exercise you did today? ")
 
 exercise_config = {
-    "query": f"{exercise_text}",
+    "query": exercise_text,
     "gender": GENDER,
     "weight_kg": WEIGHT_KG,
     "height_cm": HEIGHT_CM,
-    "age": AGE
+    'age': AGE
 }
 
-response = requests.post(url=exercise_endpoint, json=exercise_config, headers=headers)
-response.raise_for_status
+response = requests.post(url=exercise_endpoint,
+                         headers=headers, json=exercise_config)
 results = response.json()
-print(results)
+
+
+# ******************************* New Configuration *************************************
+date = dt.datetime.now().strftime("%d/%m/%Y")
+time = dt.datetime.now().strftime("%X")
 
 sheety_endpoint = "https://api.sheety.co/512493e17f966118373cd869f61856dd/workoutTracking/workouts"
+
+for exercise in results["exercises"]:
+    sheety_config = {
+        "workout": {
+            "date": date,
+            "time": time,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"]
+        }
+    }
+    res = requests.post(url=sheety_endpoint,
+                        json=sheety_config, auth=("andrews", "prayer01"))
+    res.raise_for_status()
+    print(res.text)
